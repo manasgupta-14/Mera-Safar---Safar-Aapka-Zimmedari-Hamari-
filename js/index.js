3d3b0c84b909475cfa92808a578f90a3e7a0dec9
Manas Gupta <manasmggupta@gmail.com>
2026-06-19 21:40:08 +0530

    Slider function is add in index.js

--- js/index.js
+++ js/index.js
@@ -3,6 +3,44 @@ const navLinks = document.querySelector('.nav-links');
 
 hamburger.addEventListener('click', () => {
     navLinks.classList.toggle('active');
-    
+
     hamburger.classList.toggle('toggle');
 });
+
+const slider = document.querySelector('.slider');
+const slides = document.querySelectorAll('.slide');
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+
+let currentIndex = 0;
+const totalSlides = slides.length;
+
+function updateSlider() {
+    slider.style.transform = `translateX(-${currentIndex * 100}%)`;
+}
+
+nextBtn.addEventListener('click', () => {
+    currentIndex++;
+
+    if (currentIndex >= totalSlides) {
+        currentIndex = 0;
+    }
+    updateSlider();
+});
+
+prevBtn.addEventListener('click', () => {
+    currentIndex--;
+
+    if (currentIndex < 0) {
+        currentIndex = totalSlides - 1;
+    }
+    updateSlider();
+});
+
+setInterval(() => {
+    currentIndex++;
+    if (currentIndex >= totalSlides) {
+        currentIndex = 0;
+    }
+    updateSlider();
+}, 5000);
