8b28fe9f937b8b2d58e154b6b48749abd4e8d294
Manas Gupta <manasmggupta@gmail.com>
2026-06-20 18:21:43 +0530

    Add API of trending places

--- js/index.js
+++ js/index.js
@@ -132,3 +132,68 @@ async function loadPopularPlaces() {
 }
 
 loadPopularPlaces();
+const trendingPlaces = document.getElementById("trendingPlacesCard");
+
+const loadTrendingPlaces = async () => {
+    try {
+        const response = await fetch("./api/trending-places-index.json");
+
+        if (!response.ok) {
+            throw new Error("Failed to load Trending Places API");
+        }
+
+        const data = await response.json();
+
+        const filterData = data.filter(item => [1, 4, 6].includes(item.id));
+
+        let html = "";
+
+        filterData.forEach((item) => {
+            html += `
+            <div class="package-card">
+              <div class="card-image-wrapper">
+                <img src="${item.image}" alt="${item.location} Photo" />
+              </div>
+
+              <div class="card-body">
+                <div class="card-action-bar">
+                  <div class="discount-badge">${item.discount || "20% OFF"}</div>
+                  <button class="wishlist-btn">❤️</button>
+                </div>
+
+                <h3 class="package-title">${item["package-name"]}</h3>
+
+                <div class="location-row">
+                  <img src="${item["location-icon"]}" alt="" />
+                  ${item.location}
+                </div>
+
+                <div class="meta-grid">
+                  <div class="meta-item">${item.duration}</div>
+                  <div class="meta-item">${item["package-type"]}</div>
+                  <div class="meta-item">${item["departure-day"]}</div>
+                  <div class="meta-item">⭐ ${item.rating}</div>
+                </div>
+
+                <div class="scarcity-alert">🔴 Only ${item["seat-left"]} Seats Left</div>
+              </div>
+
+              <div class="card-footer">
+                <div class="price-section">
+                  <span class="old-price"><del>${item["old-price"]}</del></span>
+                  <div class="new-price">${item["new-price"]}<small>/person</small></div>
+                </div>
+                <button class="book-now-btn">Book Now</button>
+              </div>
+            </div>
+            `;
+        });
+
+        trendingPlaces.innerHTML = html;
+
+    } catch (error) {
+        console.log("Error", error);
+    }
+};
+
+loadTrendingPlaces();
